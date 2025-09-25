3f6020f24cb30b45bbba2507159a547e3ad7de61
julfekar ali <alijulfekar419@gmail.com>
2025-09-25 07:37:34 +0000

    JOOOOO

--- 01_Basics/01_Varible.js
+++ 01_Basics/01_Varible.js
@@ -7,4 +7,11 @@ var password="12345";
  let state;
 console.table([AccountId ,email,password,accountCity,state])
  console.log(AccountId);
+
+
+//  var → old, function-scoped, avoid using it in modern code.
+
+// let → use when you expect the variable value to change.
+
+// const → use when you don’t want the variable reference to change.
  
---
+++ 01_Basics/main.js
@@ -0,0 +1,9 @@
+let a;
+console.log(a);
+ a=45;
+ const b = a;
+ console.log(a);
+ console.log(b);
+ console.log(typeof(a));
+ 
+ 
